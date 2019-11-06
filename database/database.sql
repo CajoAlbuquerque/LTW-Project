abb3271670579985166d04851d4e093032dbcf6c
Cajo Albuquerque <up201706735@fe.up.pt>
2019-11-06 10:28:20 +0000

    Database file

---
+++ database/database.sql
@@ -0,0 +1,35 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS Reservation;
+DROP TABLE IF EXISTS House;
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User (
+    userID INTEGER PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT,
+    nationality TEXT,
+    age INTEGER
+);
+
+CREATE TABLE House (
+    houseID INTEGER PRIMARY KEY,
+    title TEXT,
+    priceDay REAL,
+    location TEXT,
+    description TEXT,
+    owner INTEGER NOT NULL,
+    FOREIGN KEY (owner) REFERENCES User(userID)
+);
+
+CREATE TABLE Reservation (
+    reservationID INTEGER PRIMARY KEY,
+    user INTEGER,
+    house INTEGER,
+    startDate TEXT,
+    endDate TEXT,
+    totalPrice REAL,
+    FOREIGN KEY (user) REFERENCES User(userID),
+    FOREIGN KEY (house) REFERENCES House(houseID)
+);
