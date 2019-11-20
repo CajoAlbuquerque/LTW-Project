69b1ff26d6c2e1535e5de3b9546d0b3a6ac58ec5
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-11-20 09:59:12 +0000

    First Register page draft

---
+++ pages/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homu Register</title>
+</head>
+<body>
+    <nav></nav>
+    <header><img src="#" alt="Homu logo, a japanese arch over in over the Fuji mountain in red"></header>
+    <section id="main">
+        <p> <strong>Register Now!</strong> </p>
+        <form action="">
+            <label for="email">Email: <input type="text" id="email"></label>
+            <label for="name">Name: <input type="text" id="name"></label>
+            <label for="date">Date of Birth: <input type="date" id="date"></label>
+            <label for="pass">Password: <input type="password" id="pass"></label>
+            <label for="confirm">Confirm Password: <input type="password" id="confirm"></label>
+            <input type="submit" value="Register">
+        </form>
+    </section>
+</body>
+</html>
