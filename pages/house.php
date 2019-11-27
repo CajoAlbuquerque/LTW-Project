8a2e0d52199613151aa9b911e0f50e1cdb3d4481
BerserkingIdiot <skullventuzelos@hotmail.com>
2019-11-27 10:18:08 +0000

    started house.php

--- pages/house.php
+++ pages/house.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homu</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+    include_once('../database/db_house_info.php');
+    include_once('../database/db_user_info.php');
+    include_once('../database/db_comments.php');
+    include('../templates/temp_common.php');
+
+    $houseID = $_GET['houseID'];
+
+    $house = getHouse($houseID);
+    $owner = getUser($house['owner']);
+    $comments = getAllCommentsOfHouse($houseID);
+
+    draw_header();
+    draw_house($house, $owner, $comments);
+    draw_footer();
+?>
