994dda6dd7418d764919b0e64b387d3f399e0595
SALajes <sofialajes98@gmail.com>
2019-12-07 20:23:27 +0000

    Main header fixed

---
+++ pages/homepage.php
@@ -0,0 +1,8 @@
+<?php 
+    include('../templates/temp_common.php');
+    draw_MainHeader();
+?>
+    <link href="../css/aboutusStyle.css" rel="stylesheet" >
+<?php 
+    draw_footer();
+?>
--- templates/temp_common.php
+++ templates/temp_common.php
@@ -11,9 +11,9 @@
         <body>
             <nav id="mainHeader">
                 <ul>
-                    <li id="logo"><a href="pages/homepage.php"><img src="../Icons/logo.png" alt="Hömu logo"></a></li>
-                    <li><a href="pages/search.php"><img src="../Icons/search_black.png" alt="Search"></a></li>
-                    <li><a href="pages/profile.php"><img src="../Icons/profile_icon_black.png" alt="Profile"></a></li>
+                    <li id="logo"><a href="homepage.php"><img src="../Icons/logo.png" alt="Hömu logo"></a></li>
+                    <li><a href="search.php"><img src="../Icons/search_black.png" alt="Search"></a></li>
+                    <li><a href="profile.php"><img src="../Icons/profile_icon_black.png" alt="Profile"></a></li>
                 </ul>
             </nav>
 <?php } ?>
@@ -31,9 +31,9 @@
         <body>
             <nav id="secondaryHeader">
                 <ul>
-                    <li id="logo"><a href="pages/homepage.php"><img src="../Icons/brand_white.png" alt="Hömu"></a></li>
-                    <li><a href="pages/search.php"><img src="../Icons/search.png" alt="Search"></a></li>
-                    <li><a href="pages/profile.php"><img src="../Icons/profile_icon.png" alt="Profile"></a></li>
+                    <li id="logo"><a href="homepage.php"><img src="../Icons/brand_white.png" alt="Hömu"></a></li>
+                    <li><a href="search.php"><img src="../Icons/search.png" alt="Search"></a></li>
+                    <li><a href="profile.php"><img src="../Icons/profile_icon.png" alt="Profile"></a></li>
                 </ul>
             </nav>
 <?php } ?>
