5cc78613423f201ad7fcdb3cae56a140c8f1c087
Cajo Albuquerque <carlosjo.da@gmail.com>
2019-12-11 16:17:14 +0000

    Restriction of dates based on today's date and selected dates

--- js/search_form.js
+++ js/search_form.js
@@ -1,9 +1,18 @@
 'use strict'
 
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+
 let search_form = document.getElementById('search-form');
 let search = document.getElementById('search');
 let check_in = document.querySelector('#search-form input[name="check_in"]');
 let check_out = document.querySelector('#search-form input[name="check_out"]');
+
+function todayDate() {
+    return yyyy + '-' + mm + '-' + dd;
+}
 
 check_in.addEventListener('focus', () => check_in.type = 'date');
 check_out.addEventListener('focus', () => check_out.type = 'date');
@@ -24,3 +33,13 @@ document.addEventListener('click', function (ev) {
         search_form.style.opacity = '0';
     }
 });
+
+check_in.min = todayDate();
+
+check_in.addEventListener('change', function () {
+    check_out.min = check_in.value;
+});
+
+check_out.addEventListener('change', function () {
+    check_in.max = check_out.value;
+})
