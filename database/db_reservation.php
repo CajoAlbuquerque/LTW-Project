c6fe544ef0beb0e9a9d9722c2b41b142b1b79fc1
BerserkingIdiot <skullventuzelos@hotmail.com>
2019-12-11 09:48:49 +0000

    started db access for reservations

---
+++ database/db_reservation.php
@@ -0,0 +1,49 @@
+<?php
+    include_once('../database/connection.php');
+
+    function getReservationsOfHouse($house) {
+        global $db;
+
+        $stmt = $db->prepare('SELECT * FROM Reservation  WHERE house = ?');
+        $stmt->execute(array($house));
+        return $stmt->fetchAll();
+    }
+
+    function getReservationsOfUser($user) {
+        global $db;
+
+        $stmt = $db->prepare('SELECT * FROM Reservation  WHERE user = ?');
+        $stmt->execute(array($user));
+        return $stmt->fetchAll();
+    }
+
+    function insertReservation($user, $house, $startDate, $endDate, $totalPrice) {
+        global $db;
+
+        $stmt = $db->prepare('INSERT INTO Reservation(user, house, startDate, endDate, totalPrice) VALUES (?, ?, ?, ?, ?)');
+        $stmt->execute(array($user, $house, $startDate, $endDate, $totalPrice));
+    }
+
+    function checkValidDate($house, $startDate, $endDate) {
+        
+        if($endDate < $startDate){
+            return false;
+        }
+        
+        global $db;
+
+        $stmt = $db->prepare('SELECT * FROM Reservation WHERE house = ?');
+        $stmt->execute(array($user));
+        $reservations = $stmt->fetchAll();
+        foreach($reservations as $reservation) {
+            $resStart = $reservation['startDate'];
+            $resEnd = $reservation['endDate'];
+            if(!($resStart > $endDate || $resEnd < $startDate)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+?>
