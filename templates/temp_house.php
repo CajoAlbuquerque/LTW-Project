9c623341730b3103cb330eafaec8b1e2bbd26eaa
SALajes <sofialajes98@gmail.com>
2019-12-18 04:05:51 +0000

    house page responsive

--- templates/temp_house.php
+++ templates/temp_house.php
@@ -26,7 +26,7 @@
             </ul>
             <div class="rating"> 
                 <p><?=$rating_info['rating']?><img src="../icons/star.png">
-                <p>(<?=$rating_info['count']?>)</p>
+                <p>(<?=$rating_info['count']?> users)</p>
             </div>
         </section>
         <section class="description">
@@ -54,7 +54,7 @@
             <?php foreach ($comments as $comment) { ?>
                 <li><p><?=$comment['comment']?></p>
                     <section class="comment_footer">
-                        <span class="rating"> <?=$comment['stars']?> stars </span>
+                        <span class="rating"> <?=$comment['stars']?><img src="../icons/star.png"></span>
                         <span class="commenter">by <a href="../pages/profile.php?username=<?=$comment['username']?>"> <?=$comment['username']?> </a></span>
                     </section>
                 </li>
@@ -107,7 +107,7 @@ function draw_new_house_form() { ?>
                 </ul>
                 <div class="rating"> 
                     <p><?=$rating_info['rating']?><img src="../icons/star.png">
-                    <p>(<?=$rating_info['count']?>)</p>
+                    <p>(<?=$rating_info['count']?> users)</p>
                 </div>
             </section>
         </section>
